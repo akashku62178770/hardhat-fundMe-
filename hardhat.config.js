1f905a417fbc3a29b78dc8e75b71381907af4038
Akash Kumar <akashkr.sahebjung5@gmail.com>
2022-11-23 00:07:15 +0530

    initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,56 @@
+require("dotenv").config()
+require("@nomiclabs/hardhat-etherscan")
+require("@nomiclabs/hardhat-waffle")
+require("hardhat-gas-reporter")
+require("solidity-coverage")
+// require("")
+require("@nomicfoundation/hardhat-toolbox")
+require("hardhat-deploy")
+// const { network } = require("hardhat")
+
+/** @ import('hardhat/config').HardhatUserConfig */
+
+const RPC_URL = process.env.RPC_URL || "https://eth-rinkby/example"
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
+
+module.exports = {
+    // solidity: "0.8.8",
+    solidity: {
+        compilers: [{ version: "0.8.8" }, { version: "0.6.6" }],
+    },
+    defaultNetwork: "hardhat",
+    networks: {
+        polygon: {
+            url: RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 80001,
+            blockConfirmations: 6,
+        },
+        localhost: {
+            url: "http://127.0.0.1:8545/",
+            // accounts: Thanks to hardhat
+            chainId: 31337,
+        },
+    },
+    gasReporter: {
+        enabled: true ,
+        outputFile: "gas-report.txt",
+        noColors: true,
+        currency: "USD",
+        // coinmarketcap: COINMARKETCAP_API_KEY,
+        token: "MATIC",
+    },
+    etherscan: {
+        apiKey: process.env.ETHERSCAN_API_KEY,
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+        user: {
+            default: 1,
+        },
+    },
+}
---
+++ helper-hardhat-config.js
@@ -0,0 +1,18 @@
+const networkConfig = {
+    80001: {
+        name: "polygon",
+        ethUsdPriceFeed: "0xF9680D99D6C9589e2a93a78A04A279e509205945",
+    },
+}
+
+const developmentChains = ["hardhat", "localhost"]
+const DECIMALS = 8
+const INITIAL_ANSWER = 200000000000
+
+module.exports = {
+    networkConfig,
+    developmentChains,
+    DECIMALS,
+    INITIAL_ANSWER,
+}
+// 0xF9680D99D6C9589e2a93a78A04A279e509205945
